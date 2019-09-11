9c4bedf35e30b9e189890662a0de181beb04834a
simona1989 <825928023@qq.com>
2019-09-11 23:49:52 +0800

    俱乐部功能开发

--- src/activities/sqGame/js/club.js
+++ src/activities/sqGame/js/club.js
@@ -1,5 +1,19 @@
 (function() {
     FastClick.attach(document.body);
+    var openId = Util.getParam("openId"), // 登录后返回相关openid
+    code = Util.getParam("code"); // 授权code
+
+  /** 主要初始化 */
+    var baseInfo = Util.getSession(Util.baseInfo),
+      token = Util.getSession(Util.token);
+    if (!baseInfo ||  !token) {
+        Util.auth(code, function(token) {
+            Util.getUserInfo(function(data) {});
+            getListOfUser();
+        });
+    } 
+    // 1、进来第一步，调群组列表
+    getListOfUser();
 
     // $(".create-room-popup").show();
     $('.btn-groups').on('click', '.btn', function(e) {
@@ -48,6 +62,100 @@
         });
     });
 
+    
+
+    $('.popup-wrapper').on("click", '.masker, .icon-close, .to-close', function() {
+        $(".popup-wrapper").hide();
+    });
+
+    $(".btn-save").on("click", function(e) {
+        // location.href = "./gameAnbao.html";
+        // return;
+        var roomParams = {
+          chip: $('input[name="chips"]:checked').val(),
+          chipLimit: $('input[name="chipLimits"]:checked').val(),
+          people: $('input[name="peoples"]:checked').val(),
+          betTime: $('input[name="betTimes"]:checked').val(),
+          gameId: $('.game-list-item.active').attr('data-id'),
+          playOdds: "",
+          type: $(".btns-wapper")
+            .find(".active")
+            .eq(0)
+            .attr("data-type")
+        };
+        var $playOddsEle = $(".playOdds:checked"),
+          gameNumberRoomCard = $(
+            'input[name="gameNumberOfGameList"]:checked'
+          ).val();
+        if (gameNumberRoomCard) {
+          var arr = gameNumberRoomCard.split(",");
+          roomParams.numberOfGame = arr[0];
+          roomParams.roomCard = arr[1];
+        }
+        if ($playOddsEle) {
+          var playOdds = [];
+          // playOdds = {};
+          for (var i = 0, eleLength = $playOddsEle.length; i < eleLength; i++) {
+            // console.info("elem--", $playOddsEle.eq(i));
+            var idOdds = $playOddsEle
+              .eq(i)
+              .val()
+              .split(",");
+  
+            playOdds.push(
+              JSON.stringify({
+                gamePlayId: parseInt(idOdds[0]),
+                odds: parseFloat(idOdds[1])
+              })
+            );
+            // playOdds[idOdds[0]] = idOdds[1];
+          }
+          // roomParams.playOdds = playOdds;
+          // roomParams.playOdds = JSON.stringify(playOdds);
+          roomParams.playOdds = "[" + playOdds.join(",") + "]";
+        }
+        // console.info("roomParams---", roomParams);
+        Util.Ajax({
+          url: Util.openAPI + "/app/room/createRoom",
+          type: "post",
+          data: roomParams,
+          dataType: "json",
+          cbOk: function(data, textStatus, jqXHR) {
+            // console.log(data);
+            if (data.code === 0) {
+              // Util.setSession("roomParams", data.data);
+              var id = data.data.id;
+              location.href = "./gameAnbao.html?id=" + id;
+            } else {
+              Util.toast(data.msg);
+            }
+          },
+          cbErr: function(e, xhr, type) {
+            Util.toast("创建房间失败");
+          }
+        });
+      });
+
+      // 1、进来第一步，调群组列表接口group/getListOfUser
+    function getListOfUser() {
+        Util.Ajax({
+            url: Util.openAPI + "/app/group/getListOfUser",
+            type: "get",
+            dataType: "json",
+            cbOk: function(data, textStatus, jqXHR) {
+            // console.log(data);
+            if (data.code === 0) {
+                console.log(data.data)
+                // 下面
+            } else {
+                Util.toast("获取群组列表失败");
+            }
+            },
+            cbErr: function(e, xhr, type) {
+            Util.toast("获取群组列表失败");
+            }
+        });
+    }
     // 游戏类型
     function eventGametype() {
         getGameList(function(games) {
@@ -57,8 +165,6 @@
             $(".create-room-popup").show();
         });
     }
-
-
     //获取游戏列表
     function getGameList(cb) {
         Util.Ajax({
@@ -84,5 +190,9 @@
             Util.toast("获取游戏列表失败");
             }
         });
+    }
+
+    function eventScore() {
+        window.location.href = './record.html';
     }
 })();
